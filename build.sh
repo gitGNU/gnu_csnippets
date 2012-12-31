d596c1dd714547f5a8dc58b175d72133361e2169
Allan Ference <f.fallen45@gmail.com>
2012-12-31 20:50:47 +0000

    build.sh: clean it up and make sure we're really inside git to generate version

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@
 CORES=`grep processor /proc/cpuinfo | wc -l`
 # Process to be passed to $MAKE
 MAKEOPT=$(($CORES + 1))
-# arguments to pass to the executable generated
+# arguments to pass to $TARGET (see below) generated
 ARGS=1337
 # The target to build after building the static library.
 TARGET=server
@@ -22,18 +22,25 @@ DBG=gdb
 buildopt=
 # whether we should clean before building
 clean_before_build=no
+# Are we cross-compiling?
+cross_build=no
 
 # If this is the GIT version, define build commit and revision.
 # Otherwise, don't do anything.  We might need the
-# gen-version.sh script for releases, this will help
+# gen-version.sh script instead of all this magic, this will help
 # get the tag name and version associated.
 # Or alternatively, we could write an else case here, and
 # define those strings to something informative.
 #
 # Those are intended for feature-checking in the near future.
 if [ -d .git ]; then
-	buildopt="-DBUILD_COMMIT=`git describe --dirty --always` \
-			-DBUILD_REVISION=`git rev-list --all | wc -l`"
+	really_inside=yes
+	git --help >/dev/null  # Is GIT actually installed at this site?
+	test $? -eq 0 || really_inside=no
+	if [ "$really_inside" = "yes" ]; then
+		buildopt="$buildopt -DBUILD_COMMIT=`git describe --dirty --always` \
+			   -DBUILD_REVISION=`git rev-list --all | wc -l`"
+	fi
 fi
 
 run() {
@@ -59,13 +66,35 @@ _make() {
 }
 
 case "$2" in
-	-s) buildopt=-DUSE_SELECT_HANDLER=ON ;;
+	-s) buildopt="$buildopt -DUSE_SELECT_HANDLER=ON" ;;
 	-c) clean_before_build=yes ;;
-	*) buildopt=-DUSE_SELECT_HANDLER=OFF ;;
+	-cb) cross_build=yes ;;
+	*) buildopt="$buildopt -DUSE_SELECT_HANDLER=OFF" ;;
 esac
+
+if [ "$cross_build" = "yes" ]; then
+	if [ -d build ]; then
+		# CMake does not allow major changes like these if there's
+		# already a cached version with these variables set, so clean it up.
+		rm -rf build
+	fi
+	buildopt="$buildopt -DCMAKE_SYSTEM_NAME=Windows\
+		   -DCMAKE_RC_COMPILER=/usr/bin/i486-mingw32-windres\
+		   -DCMAKE_C_COMPILER=/usr/bin/i486-mingw32-gcc\
+		   -DCMAKE_CXX_COMPILER=/usr/bin/i486-mingw32-g++"
+fi
+
+if [ "$clean_before_build" = "yes" ]; then
+	if [ -d build ]; then
+		echo "Removing build directory..."
+		rm -rf build
+	fi
+fi
 
 mkdir -p build
 cd build
+
+echo Generating Makefile with $buildopt
 cmake .. $buildopt || exit
 
 if [ "$clean_before_build" = "yes" ]; then
